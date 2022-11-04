54079de4b3801e609560e2541be7cb384fceaa38
Braydan Brown <73270460+BraydanBrown@users.noreply.github.com>
2022-11-04 02:21:30 -0400

    modified views, partials, routes, app.css
    - Modified index.ejs to include a splash page. Created button which links to the crud.ejs page.
    - Implemented partials: bottom_nav.ejs, footer.ejs, header.ejs, main_nav.ejs.
    - Various style changes in app.css.
    - Created new routes in index.js

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// const mongoose = require('mongoose'); //mongoose
 
-var app = express();
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+let app = express();
+
+// mongoose.connect()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Splash' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Splash' });
+});
+
+router.get('/crud', function(req, res, next) {
+  res.render('crud', { title: 'CRUD' });  
 });
 
 module.exports = router;
